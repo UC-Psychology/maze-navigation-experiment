a04b4e0ad89ae2bcd0d0973f6d376b715861a6a9
trentwirth <62706609+trentwirth@users.noreply.github.com>
2023-10-30 23:33:00 -0400

    added in the control condition

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -1,32 +1,55 @@
 using UnityEngine;
+using TMPro;
 
 public class CameraSwitch : MonoBehaviour
 {
     public Camera firstPersonCamera;
     public Camera birdsEyeCamera;
+    public Camera routeCamera;
 
     public GameObject targetColorSquare;
     public GameObject startingColorSquare;
 
     public PlayerMovementHandler playerMovementHandler;
+    public TMP_Text birdsEyeViewInstructions;
+    public TMP_Text routeViewInstructions;
+    public float conditionViewDuration = 30f;
 
     public Countdown countdown;
 
     public void EnableBirdsEyeView()
     { 
         firstPersonCamera.enabled = false;
+        routeCamera.enabled = false;
         birdsEyeCamera.enabled = true;
+        birdsEyeViewInstructions.gameObject.SetActive(true);
+        routeViewInstructions.gameObject.SetActive(false);
         playerMovementHandler.DisablePlayerMovement();
         targetColorSquare.SetActive(true);
         startingColorSquare.SetActive(true);
         countdown.StartCountdown();
-        Invoke("EnableFirstPersonView", 15f);
+        Invoke("EnableFirstPersonView", conditionViewDuration);
+    }
+
+    public void EnableRouteView()
+    {   
+        birdsEyeCamera.enabled = false;
+        routeCamera.enabled = true;
+        firstPersonCamera.enabled = false;
+        birdsEyeViewInstructions.gameObject.SetActive(false);
+        routeViewInstructions.gameObject.SetActive(true);
+        playerMovementHandler.DisablePlayerMovement();
+        countdown.StartCountdown();
+        Invoke("EnableFirstPersonView", conditionViewDuration);
     }
 
     public void EnableFirstPersonView()
     {
         birdsEyeCamera.enabled = false;
+        routeCamera.enabled = false;
         firstPersonCamera.enabled = true;
+        birdsEyeViewInstructions.gameObject.SetActive(false);
+        routeViewInstructions.gameObject.SetActive(false);
         playerMovementHandler.EnablePlayerMovement();
         targetColorSquare.SetActive(false);
     }
--- Assets/Scripts/ConditionHandler.cs
+++ Assets/Scripts/ConditionHandler.cs
@@ -2,44 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UXF;
+using TMPro;
 
 public class ConditionHandler : MonoBehaviour
 {
-    public GameObject instructionsScreen; // Assuming you have a UI screen for instructions
     public Camera mainCamera; // Reference to your main camera
     public CameraSwitch cameraSwitch;
+    public TMP_Text goInstructions;
+    public PlayerMovementHandler playerMovementHandler;
 
     public void SelectCondition(Session session)
     {
-        int conditionNumber = 2; //Random.Range(1, 4);
+        int conditionNumber = Random.Range(1, 4);
         session.settings.SetValue("condition_number", conditionNumber);
         Debug.Log(session.settings.GetInt("condition_number"));
 
         if (conditionNumber == 2)
         {
             cameraSwitch.EnableBirdsEyeView();
-            HandleBirdsEyeViewCondition();
         }
         else if (conditionNumber == 3)
         {
-            HandleWrittenDirectionsCondition();
+            cameraSwitch.EnableRouteView();
         }
-    }
-
-    void HandleWrittenDirectionsCondition()
-    {
-        // Display instructions:
-        // e.g., instructionsScreen.SetActive(true);
-
-        // Then wait for a button press to start the experiment
-    }
-
-    void HandleBirdsEyeViewCondition()
-    {
-  
-        if (Input.GetKeyDown(KeyCode.P))
+        else
         {
-            cameraSwitch.EnableFirstPersonView();
+            goInstructions.gameObject.SetActive(true);
+            playerMovementHandler.EnablePlayerMovement();
+            Invoke("DisableGoInstructions", 1f);
         }
+    }
+
+    private void DisableGoInstructions()
+    {
+        Debug.Log("Disabling go instructions");
+        goInstructions.gameObject.SetActive(false);
     }
 }
